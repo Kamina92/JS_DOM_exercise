812be1ccd0dbf5e4a7a2b49523e51568815eadef
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-05 16:01:17 +0200

    esercizio 11 fatto

---
+++ es11.js
@@ -0,0 +1,37 @@
+// Scrivi una funzione che dato un numero intero (massimo 9999) conti da quante cifre è formato.
+
+// Esempi:
+
+//     Input : 9
+//     Output :  1 cifra
+
+
+//     Input : 99
+//     Output :  2 cifre
+
+
+
+function cifre(num) {
+
+    let a = 0;
+    num = num /=10;
+    
+    if (num<1) {
+         a = 1;
+    } 
+    else if (num<10) {
+        a = 2;
+    }
+    else if (num<100) {
+        a = 3;
+    }   
+    else if (num<1000) {
+        a = 4;
+    }
+    return a
+} 
+
+
+let a = cifre(9999);
+
+console.log(a);
