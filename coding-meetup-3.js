1f9923d8a979a21607d85345ce0c79ebb664f59c
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-10 17:28:56 +0200

    esercizio codewars coding-meetup-3

---
+++ coding-meetup-3.js
@@ -0,0 +1,40 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+//   { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+// ];
+// your function should return true.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+
+
+let list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Javascript' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+
+
+let filter = list1.filter((developer) => developer.language == 'Ruby');
+
+console.log(filter.length);
+
+// if (filter.length = 0){
+//   return false
+// } else return true;
+
+// function isRubyComing(list) {
+
+
+// }
