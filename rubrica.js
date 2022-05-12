013d998559a087ea6a315653c8194421a65496e1
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-12 18:12:46 +0200

    tasto add e remove contatti ok

--- rubrica.js
+++ rubrica.js
@@ -9,6 +9,12 @@ let srcContact = document.querySelector('#srcContact')
 
 let contactWrapper = document.querySelector('#contactWrapper')
 
+// Input
+
+let inputName = document.querySelector('#inputName');
+let inputNumber = document.querySelector('#inputNumber');
+
+
 // Verifica
 
 let verifica = false;
@@ -43,20 +49,83 @@ let rubrica = {
             contactWrapper.appendChild(div);
             
         });
+    },
 
-    }
+    addContact : function (nome,numero) {
+            
+        this.contatcs.push({phoneName : nome, phoneNumber : numero});
+        this.showContacts();  
+        
+    },
+
+    delContact :function(nome) {
+        
+        let filtered = this.contatcs.filter((contact)=>
+            contact.phoneName.toLowerCase() == nome.toLowerCase() )[0];
+        let index = this.contatcs.indexOf(filtered);
+        if(index>=0){
+            this.contatcs.splice(index,1);
+        }
+        this.showContacts();
+
+    },
+
 }
+
+
 
 showContacts.addEventListener('click', ()=>{
     if(verifica == false){
         showContacts.innerHTML = 'Nascondi contatti'
         rubrica.showContacts();
+        showContacts.classList.add('btnCustomActive')
         verifica = true;
-    } else if(verifica = true){
+    } else if(verifica == true){
         showContacts.innerHTML = 'Mostra contatti'
         contactWrapper.innerHTML ='';
+        showContacts.classList.remove('btnCustomActive')
         verifica = false;
     }  
+})
+
+
+
+
+addContact.addEventListener('click', ()=>{
+console.dir(inputNumber);
+    if(inputName.value !='' && inputNumber.value != ''){
+        rubrica.addContact(inputName.value,inputNumber.value);
+        if(verifica == false){
+            showContacts.innerHTML = 'Nascondi contatti';
+            showContacts.classList.add('btnCustomActive');
+            verifica = true;
+        }
+        
+    } else if (inputName.value != '' && inputNumber.value == ''){
+        alert('Non hai inserito un numero')
+    }else if (inputName.value =='' && inputNumber.value !=''){
+        alert('Non hai inserito un nome')
+    }else if (inputName.value =='' && inputNumber.value == ''){
+        alert('Non hai inserito un nome e numero')
+    }
+    
+        inputName.value = '';
+        inputNumber.value = '' ;
+})
+
+
+
+
+delContact.addEventListener('click', ()=>{
+
+    rubrica.delContact(inputName.value);
+    if(verifica == false){
+        showContacts.innerHTML = 'Nascondi contatti';
+        showContacts.classList.add('btnCustomActive');
+        verifica = true;
+    }
+    inputName.value = '';
+
 })
 
 
